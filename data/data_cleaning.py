af92c51bd88f3bef611e01537d601ab0c4b70478
kman2022 <116604620+kman2022@users.noreply.github.com>
2023-03-28 07:36:49 +0200

    udate

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -18,7 +18,7 @@ IQ = 'queues_2021_clean_data.xlsx'
 dt_cols = ['q_date','wd_date','ia_date','on_date','prop_date']
 df_iq = pd.read_excel(LOC_PATH+IQ,sheet_name='data',parse_dates=dt_cols)
 
-# q_date = date project entered queue; 24,303
+# q_date = date project entered queue; 24,380
 df_iq['q_date'] = pd.to_datetime(df_iq['q_date'],errors ='coerce') #23,694 or 97.5%
 # ia_date = date of signed interconnection agreement (if available); 2,526
 df_iq['ia_date'] = pd.to_datetime(df_iq['ia_date'],errors ='coerce') # 2,524
@@ -64,6 +64,11 @@ volts = df_iq["poi_name"].str.extract(r'(\d+)\s[kK][vV]|(\d+)[kK][vV]', expand=F
 volts['sum'] = volts[0]+volts[1]
 df_iq['ix_voltage'] = volts['sum']
 
+# create the difference in months col WD Request to withdraw date∫
+df_iq['diff_months_wd']=(df_iq['wd_date']-df_iq['q_date'])/np.timedelta64(1,'M')
+
+# df_iq.to_csv('df_iq_clean.csv')
+
 # what is the diff between region(24,381) and entity(24,381)
 df_iq['region'].unique()# 9 
 df_iq['entity'].nunique() # 42
@@ -79,16 +84,28 @@ df_trend = df_trend[(df_trend['q_year']>=2000)&(df_trend['q_year']<=2016)&(df_tr
 # df_trend.groupby(['q_status']).count()
 # x = 464+2682+35+6709
 # 2682/x # ties out
-df_trend.to_csv('df_trend.csv')
+# df_trend.to_csv('df_trend.csv')
 ## bin by online year
 ## toggle by technology
 ## toggle by location
 
-# TIME to IA/COD
-df_trend_ia = df_iq[['q_year','q_status','cod_year','type_clean','mw1','region','ix_voltage','diff_months_ia','diff_months_cod']]
-df_trend_ia = df_trend_ia[(df_trend_ia['q_year']>=2000)&(df_trend_ia['q_year']<=2016)&(df_trend_ia['cod_year']<=2021)]
-df_trend_volt = df_trend_ia.groupby(['q_status']).mean(['diff_months_ia','diff_months_cod']) # 24.5 months to ia and 39 months to cod 
-df_trend_ia.to_csv('df_trend_ia.csv')
+# TIME to IA/COD/WD
+# WD: All mkts
+# IA: ERCOT, MISO, NYISO
+# ON: CAISO, ERCOT, NYISO, PJM, WEST
+df_trend_dur = df_iq[['q_year','q_status','cod_year','type_clean','mw1','region','ix_voltage','diff_months_ia','diff_months_cod','diff_months_wd']]
+# df_trend_dur = df_trend_dur[(df_trend_dur['q_year']>=2000)&(df_trend_dur['q_year']<=2016)&(df_trend_dur['cod_year']<=2021)]
+# df_trend_dur.to_csv('df_trend_dur.csv')
+# seems some projects get withdrawn then placed back in queue
+df_trend_sum = df_trend_dur[['q_status','diff_months_ia','diff_months_cod','diff_months_wd']]
+df_trend_sum = df_trend_sum.groupby(['q_status']).mean(['diff_months_ia','diff_months_cod','diff_months_wd']) # 24.5 months to ia and 39 months to cod 
+df_trend_sum
+
+
+
+
+
+
 
 # import FIPS to match the geolocs
 df_fips = pd.read_csv('fips.csv',dtype=str)
@@ -116,11 +133,12 @@ df_iq_geo.to_csv('df_iq_geo.csv')
 # active 6629/8151 or 18% did not join
 # manually mapped around 200 in the maerkets for which we have cost data 3970/4100 so 96.8% active mapped in those markets
 # df_iq_geo[(df_iq_geo['q_status']=='active')&(df_iq_geo['region'].isin(['PJM','MISO','NYISO']))].info()
+df_iq_geo[(df_iq_geo['q_status']=='operational')&(df_iq_geo['region'])].info()
 
-# df_iq_geo_unmatch = df_iq_geo[['q_id','q_year','state','county_1','region','type_clean','q_status','geometry']]
-# df_iq_geo_unmatch = df_iq_geo_unmatch[(df_iq_geo_unmatch['q_year']>2016)&(df_iq_geo_unmatch['geometry'].isnull())]
-# df_iq_geo_unmatch[df_iq_geo_unmatch['region']=='MISO']
-# df_iq_geo_unmatch.groupby(['region','q_status']).count()
+df_iq_geo_unmatch = df_iq_geo[['q_id','q_year','state','county_1','region','type_clean','q_status','geometry']]
+df_iq_geo_unmatch = df_iq_geo_unmatch[(df_iq_geo_unmatch['q_year']>2016)&(df_iq_geo_unmatch['geometry'].isnull())&(df_iq_geo_unmatch['q_status']=='operational')]
+df_iq_geo_unmatch[df_iq_geo_unmatch['region']=='MISO']
+df_iq_geo_unmatch.groupby(['region','q_status']).count()
 
 
 
